2e385c65f1e6c42831cd16b1326a100d11eb3f5c
SeanFuller294 <s.fuller294@hotmail.com>
2019-08-23 13:16:13 -0600

    Template Done

--- app/Models/List.js
+++ app/Models/List.js
@@ -1,4 +1,36 @@
 export default class List {
     //TODO You will need to create a constructor 
     //and the methods needed to create the view template for this model
+    //NOTE from sean - I think I have done this with getTemplate() and drawTasks()
+    constructor(data) {
+        this.title = data.title
+        this.tasks = data.tasks || []
+    }
+    getTemplate(index) {
+        let template = `
+        <div class = "col-4">
+      <h1>${this.title}</h1>
+      <ul>`
+        template += this.drawTasks(index)
+        template += `
+    <form onsubmit="app.controllers.listController.addTaks(event, ${index})">
+    <div class="form-group">
+                <label for="task"></label>
+                <input type="text" class="form-control" name="task" placeholder="Enter Task" required>
+                </div>
+                <button class="btn btn-success"type="submit">Add Task</button>
+                </form>
+      <button class="btn btn-danger" onclick="app.controllers.listController.deleteList(${index})">DELETE LIST</button>
+      </div>
+        `
+        return template
+    }
+
+    drawTasks(index) {
+        let taskTemplate = ""
+        this.tasks.forEach((task, taskIndex) => {
+            taskTemplate += `<li>${task} <button class="btn btn-danger"onclick="app.controllers.dessertController.deleteTopping(${index},${taskIndex})">X</button></li>`
+        });
+        return taskTemplate
+    }
 }
