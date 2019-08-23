62592e0681b3e2f415e274f392b1eeb07efada4a
SeanFuller294 <s.fuller294@hotmail.com>
2019-08-23 13:25:53 -0600

    Finished basic Service?

--- app/Services/ListService.js
+++ app/Services/ListService.js
@@ -11,7 +11,33 @@ export default class ValuesService {
     //TODO  Here is where we handle all of our data manipulation, 
     //given the information you need in the controller, 
     //what methods will be required to support that functionality?
+    constructor() {
+        this.getLists()
+    }
 
+    addTask(newTask, listIndex) {
+        _state.lists[listIndex].tasks.push(newTask)
+        this.saveLists()
+    }
+
+    deleteTask(listIndex, taskIndex) {
+        _state.lists[listIndex].tasks.splice(taskIndex, 1)
+        this.saveLists()
+    }
+
+    addList(newList) {
+        _state.lists.push(new List(newList))
+        this.saveLists()
+    }
+
+    deleteList(index) {
+        _state.lists.splice(index, 1)
+        this.saveLists()
+    }
+
+    get List() {
+        return _state.lists.map(list => new List(list))
+    }
 
 
 
